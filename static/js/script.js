a7cde38587827bdbbc8e4e99589f56039c65dce4
LucaVas <luca.vassos@gmail.com>
2023-03-19 15:51:05 +0200

    enabled feature of filter by search (#3)

--- static/js/script.js
+++ static/js/script.js
@@ -45,29 +45,57 @@ function filterNotesByTag(e) {
 
 }
 
-// apply event listener to all tags
-for (let i = 0; i < tags.length; i++) {
-    tags[i].addEventListener("click", filterNotesByTag);
+function filterByInput(e) {
+    filter = e.target.value.toLowerCase();
+
+    // make all notes visible
+    for (z = 0; z < notes.length; z++) {
+        notes[z].style.display = "flex";
+    }
+
+    // loop through each note
+    for (i = 0; i < notes.length; i++) {
+
+        noteStatus = notes[i].querySelector(".scroll-bar-note-header").children[2].value;
+        title = notes[i].querySelector(".scroll-bar-note-title").innerHTML.toLowerCase();
+        description = notes[i].querySelector(".scroll-bar-note-description").innerHTML.toLowerCase();
+
+        // check if input value is included in title or description
+        if (title.includes(filter) || description.includes(filter)) {
+            // do nothing
+        } else {
+            // hide note
+            notes[i].style.display = "none";
+        }
+    }
 }
 
-
-// change color of note if archived or deleted
-for (i = 0; i < notes.length; i++) {
-    if (notes[i].firstElementChild.childNodes[5].value == "deleted") {
-        notes[i].style.backgroundColor = "rgba(255, 0, 0, 0.308)";
+document.addEventListener("DOMContentLoaded", function() {
+    // apply event listener to all tags
+    for (let i = 0; i < tags.length; i++) {
+        tags[i].addEventListener("click", filterNotesByTag);
     }
-    else if (notes[i].firstElementChild.childNodes[5].value == "archived") {
-        notes[i].style.backgroundColor = "rgba(232, 197, 71, 0.286)";
+
+
+    // change color of note if archived or deleted
+    for (i = 0; i < notes.length; i++) {
+        if (notes[i].firstElementChild.childNodes[5].value == "deleted") {
+            notes[i].style.backgroundColor = "rgba(255, 0, 0, 0.308)";
+        }
+        else if (notes[i].firstElementChild.childNodes[5].value == "archived") {
+            notes[i].style.backgroundColor = "rgba(232, 197, 71, 0.286)";
+        }
     }
-}
 
-// remove delete button
-for (i = 0; i < notes.length; i++) {
-    if (notes[i].firstElementChild.childNodes[5].value == "deleted") {
-        document.querySelector(".delete-note-btn").style.display = "none";
-        document.querySelector("#scroll-bar-note-edit-icon").style.display = "none";
+    // remove delete button
+    for (i = 0; i < notes.length; i++) {
+        if (notes[i].firstElementChild.childNodes[5].value == "deleted") {
+            document.querySelector(".delete-note-btn").style.display = "none";
+            document.querySelector("#scroll-bar-note-edit-icon").style.display = "none";
+        }
     }
-}
 
-
-
+    // filter notes by input
+    document.querySelector("#input-search").onkeyup = filterByInput;
+    }
+);
